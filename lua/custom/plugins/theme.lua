e37b120f8c551899af54a476e7913b0ef62ee8b8
Erdragh <mail@erdragh.dev>
2024-10-16 15:47:06 +0200

    Switch to light theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -3,7 +3,7 @@ return {
     'EdenEast/nightfox.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'carbonfox'
-    end
-  }
+      vim.cmd.colorscheme 'dawnfox'
+    end,
+  },
 }
